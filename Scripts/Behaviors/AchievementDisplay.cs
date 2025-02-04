c999e0191a739de19007336a838679df0a2ccce0
EvroDev <78891194+EvroDeveloper@users.noreply.github.com>
2025-02-04 10:15:10 -0500

    AchievementDisplay

---
+++ Scripts/Behaviors/AchievementDisplay.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+namespace CustomCampaignTools.SDK
+{
+    public class AchievementDisplay : MonoBehaviour
+    {
+        public void SetupReferences(Button nextButton, Button backButton, TMP_Text pageText) { }
+        public void Activate() { }
+    }
+}
