32d7758c6e9bce3251d26d7a43e65c2560e68d3f
EvroDev <78891194+EvroDeveloper@users.noreply.github.com>
2025-02-04 10:13:13 -0500

    AchievementReferenceHolder

---
+++ Scripts/Behaviors/AchievementReferenceHolder.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace CustomCampaignTools.SDK
+{
+    public class AchievementReferenceHolder : MonoBehaviour
+    {
+        public void SetReferences(Image img, TMP_Text title, TMP_Text description) { }
+    }
+}
