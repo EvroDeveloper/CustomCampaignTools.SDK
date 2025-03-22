a151ee25265583ec8b8a1c32bc03aa2a586e6b4f
EvroCraft <evro.minecraft@gmail.com>
2025-03-21 22:08:50 -0400

    Update 0.2.2

--- Scripts/Editor/CampaignSettingsEditor.cs
+++ Scripts/Editor/CampaignSettingsEditor.cs
@@ -4,13 +4,15 @@ using UnityEngine.UIElements;
 using UnityEditor.UIElements;
 using CustomCampaignTools.SDK;
 using Unity.Plastic.Antlr3.Runtime.Tree;
+using WebSocketSharp;
 
 [CustomEditor(typeof(CampaignSettings))]
 public class CampaignSettingsEditor : Editor
 {
     VisualElement Restrict;
     VisualElement Whitelist;
-    VisualElement BodylogToggle;
+    Toggle BodylogToggle;
+    Button ExportButton;
 
     new CampaignSettings target;
 
@@ -26,24 +28,56 @@ public class CampaignSettingsEditor : Editor
         VisualElement tree = visualTree.Instantiate();
 
         DropdownField AvatarRestriction = tree.Q<DropdownField>("AvatarRestriction");
+        Restrict = tree.Q<VisualElement>("RestrictionType-Restrict");
+        Whitelist = tree.Q<VisualElement>("RestrictionType-Whitelist");
+        BodylogToggle = tree.Q<Toggle>("DisableBodyLog");
 
         AvatarRestriction.RegisterValueChangedCallback(choice => OnRestrictionChoiceChanged(choice.newValue));
 
+        if (target.AvatarRestriction.HasFlag(AvatarRestrictionType.DisableBodyLog))
+        {
+            BodylogToggle.value = true;
+        }
+
         if (target.AvatarRestriction == AvatarRestrictionType.None)
+        {
             AvatarRestriction.index = 0;
+            OnRestrictionChoiceChanged(AvatarRestriction.choices[0]);
+        }
         else if (target.AvatarRestriction.HasFlag(AvatarRestrictionType.EnforceWhitelist))
+        {
             AvatarRestriction.index = 2;
+            OnRestrictionChoiceChanged(AvatarRestriction.choices[2]);
+        }
         else if (target.AvatarRestriction.HasFlag(AvatarRestrictionType.RestrictAvatar))
+        {
             AvatarRestriction.index = 1;
+            OnRestrictionChoiceChanged(AvatarRestriction.choices[1]);
+        }
         else
         {
             AvatarRestriction.index = 0;
             target.AvatarRestriction = AvatarRestrictionType.None;
+            OnRestrictionChoiceChanged(AvatarRestriction.choices[0]);
         }
 
-        Restrict = tree.Q<VisualElement>("RestrictionType-Restrict");
-        Whitelist = tree.Q<VisualElement>("RestrictionType-Whitelist");
-        BodylogToggle = tree.Q<VisualElement>("DisableBodyLog");
+        BodylogToggle.RegisterCallback<ChangeEvent<bool>>((evt) =>
+        {
+            if (evt.newValue)
+            {
+                target.AvatarRestriction |= AvatarRestrictionType.DisableBodyLog;
+            }
+            else
+            {
+                target.AvatarRestriction &= ~AvatarRestrictionType.DisableBodyLog;
+            }
+        });
+
+        ExportButton = tree.Q<Button>("ExportButton");
+        ExportButton.RegisterCallback<MouseUpEvent>((evt) =>
+        {
+            target.SaveCampaignJson();
+        });
 
         root.Add(tree);
 
@@ -72,6 +106,16 @@ public class CampaignSettingsEditor : Editor
                 Whitelist.style.display = DisplayStyle.Flex;
                 BodylogToggle.style.display = DisplayStyle.Flex;
                 break;
+        }
+
+
+        if (BodylogToggle.value)
+        {
+            target.AvatarRestriction |= AvatarRestrictionType.DisableBodyLog;
+        }
+        else
+        {
+            target.AvatarRestriction &= ~AvatarRestrictionType.DisableBodyLog;
         }
     }
 }
