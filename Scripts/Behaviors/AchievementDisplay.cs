7f613492aaed2776083d853ca46ba98c20007a63
EvroDev <78891194+EvroDeveloper@users.noreply.github.com>
2025-05-08 11:25:44 -0400

    Public stuff in Achievement Display

--- Scripts/Behaviors/AchievementDisplay.cs
+++ Scripts/Behaviors/AchievementDisplay.cs
@@ -7,10 +7,10 @@ namespace CustomCampaignTools.SDK
     [AddComponentMenu("CustomCampaignTools/Achievements/Achievement Display")]
     public class AchievementDisplay : MonoBehaviour
     {
-        private Button nextButton;
-        private Button backButton;
-        private TMP_Text pageText;
-        private TMP_Text unlockCount;
+        public Button nextButton;
+        public Button backButton;
+        public TMP_Text pageText;
+        public TMP_Text unlockCount;
         
         public void Activate() { }
     }
